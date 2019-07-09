32b735d8634aafda488ab3eacfb93f25697a2d6d
EdisonStalin <edison.jumbo@epn.edu.ec>
2019-07-09 11:50:51 -0500

    firts branch

---
+++ Pets-Project/src/Form/MascotaType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Mascota;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MascotaType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('Type')
+            ->add('age')
+            ->add('raza')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Mascota::class,
+        ]);
+    }
+}
