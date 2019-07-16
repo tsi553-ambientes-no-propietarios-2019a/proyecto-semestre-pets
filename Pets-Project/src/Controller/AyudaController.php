d1e7ac53ff100d1f78e11410ff17322d41d58123
00JOSExyz00 <jose.cortez@epn.edu.ec>
2019-07-16 18:51:02 -0500

    ayuda

---
+++ Pets-Project/src/Controller/AyudaController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AyudaController extends AbstractController
+{
+    /**
+     * @Route("/ayuda", name="ayuda")
+     */
+    public function index()
+    {
+        return $this->render('ayuda/index.html.twig', [
+            'controller_name' => 'AyudaController',
+        ]);
+    }
+}
