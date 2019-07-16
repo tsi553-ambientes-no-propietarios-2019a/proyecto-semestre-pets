91fb2d83955cf958af470dd236d28cbbc098bf6e
00JOSExyz00 <jose.cortez@epn.edu.ec>
2019-07-16 17:38:28 -0500

    Loguiversion 1.0

--- Pets-Project/src/Entity/User.php
+++ Pets-Project/src/Entity/User.php
@@ -1,9 +1,11 @@
 <?php
 // src/Entity/User.php
+
 namespace App\Entity;
+
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
+
 /**
  * @ORM\Entity
  * @ORM\Table(name="fos_user")
@@ -17,21 +19,9 @@ class User extends BaseUser
      */
     protected $id;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $name;
-
-    public function getName(): ?string
+    public function __construct()
     {
-        return $this->name;
+        parent::__construct();
+        // your own logic
     }
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-    
-} 
+}
